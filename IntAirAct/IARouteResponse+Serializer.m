912fdfa36429a8b519051a880d086bfe20cfd3ad
Arlo O'Keeffe <MailToArlo@googlemail.com>
2012-05-05 22:24:06 -0600

    Added array handling to respondWith

--- IntAirAct/IARouteResponse+Serializer.m
+++ IntAirAct/IARouteResponse+Serializer.m
@@ -13,18 +13,42 @@ static const int intAirActLogLevel = IA_LOG_LEVEL_INFO; // | IA_LOG_FLAG_TRACE;
 
 -(void)respondWith:(id)data withIntAirAct:(IAIntAirAct *)intAirAct
 {
-    RKObjectSerializer* serializer = [intAirAct serializerForObject:data];
-    
     NSError * error = nil;
-    id params = [serializer serializationForMIMEType:intAirAct.defaultMimeType error:&error];
+    NSString * response;
     
+    if([data isKindOfClass:[NSArray class]]) {
+        RKObjectSerializer * serializer;
+        id serialized = [NSMutableArray new];
+        for(id entry in data) {
+            serializer = [intAirAct serializerForObject:entry];
+            id serializedObject = [serializer serializedObject:&error withRootKeyPath:NO];
+            if(error) {
+                IALogWarn(@"Could not serialize Object: %@", entry);
+                continue;
+            }
+            [serialized addObject:serializedObject];
+        }
+        if(serializer && serializer.mapping.rootKeyPath) {
+            serialized = [NSDictionary dictionaryWithObjectsAndKeys:serialized, serializer.mapping.rootKeyPath, nil];
+        }
+        id<RKParser> parser = [[RKParserRegistry sharedRegistry] parserForMIMEType:intAirAct.defaultMimeType];
+        response = [parser stringFromObject:serialized error:&error];
+    } else {
+        RKObjectSerializer* serializer = [intAirAct serializerForObject:data];
+        id serializedObject = [serializer serializedObject:&error];
+        if(!error) {
+            id<RKParser> parser = [[RKParserRegistry sharedRegistry] parserForMIMEType:intAirAct.defaultMimeType];
+            response = [parser stringFromObject:serializedObject error:&error];
+        }
+    }
+        
     if (error) {
         self.statusCode = 500;
         IALogError(@"Serializing failed for source object %@: %@", data, [error localizedDescription]);
     } else {
         self.statusCode = 200;
-        [self respondWithData:[params data]];
-        IALogInfo(@"%@: Serialization of data: %@", THIS_FILE, [[NSString alloc] initWithData:[params data] encoding:NSUTF8StringEncoding]);
+        [self respondWithString:response];
+        IALogInfo(@"%@: Serialization of data: %@", THIS_FILE, response);
     }
 }
 
