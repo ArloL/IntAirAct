8a2c31daddd705e0a84f681a59958216a3271cb1
Arlo O'Keeffe <mailtoarlo@gmail.com>
2012-10-17 17:55:54 -0600

    Added IARouter protocol

---
+++ IntAirAct/IARouter.h
@@ -0,0 +1,14 @@
+#import <Foundation/Foundation.h>
+
+@class IARoute;
+@class IARequest;
+@class IAResponse;
+
+typedef void (^IARequestHandler)(IARequest *request, IAResponse *response);
+
+@protocol IARouter <NSObject>
+
+// returns NO when the route already exists
+-(BOOL)route:(IARoute*)route withHandler:(IARequestHandler)handler;
+
+@end
