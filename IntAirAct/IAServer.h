870b1be4ea7adf19091156283b2e302960778b52
Arlo O'Keeffe <mailtoarlo@gmail.com>
2012-10-18 12:33:42 -0600

    Renamed IARouter to IAServer

--- IntAirAct/IAServer.h
+++ IntAirAct/IAServer.h
@@ -4,9 +4,16 @@
 
 typedef void (^IARequestHandler)(IARequest *request, IAResponse *response);
 
-@protocol IARouter <NSObject>
+@protocol IAServer <NSObject>
+
+/** The port on which to listen on. Default is 0. This means the system will find a free port. */
+@property (nonatomic) NSInteger port;
 
 // returns NO when the route already exists
 -(BOOL)route:(IARoute*)route withHandler:(IARequestHandler)handler;
+
+-(BOOL)start:(NSError **)errPtr;
+
+-(void)stop;
 
 @end
