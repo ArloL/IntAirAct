217acb1f284db1664032ce2e9e0ad65690512513
Arlo O'Keeffe <mailtoarlo@gmail.com>
2012-10-18 20:35:55 -0600

    Added handlers for lost services and errors

--- IntAirAct/NSNotificationCenter+ServiceFound.h
+++ IntAirAct/NSNotificationCenter+ServiceFound.h
@@ -1,10 +1,16 @@
 #import "SDService.h"
 
 typedef void (^SDServiceHandler)(SDService * service);
+typedef void (^SDErrorHandler)(NSDictionary * service);
 
 @interface NSNotificationCenter (ServiceFound)
 
 +(id)addHandlerForServiceFound:(SDServiceHandler)handler;
++(id)addHandlerForServiceLost:(SDServiceHandler)handler;
++(id)addHandlerForServiceDiscoveryError:(SDErrorHandler)handler;
+
 -(id)addHandlerForServiceFound:(SDServiceHandler)handler;
+-(id)addHandlerForServiceLost:(SDServiceHandler)handler;
+-(id)addHandlerForServiceDiscoveryError:(SDErrorHandler)handler;
 
 @end
--- IntAirAct/NSNotificationCenter+ServiceFound.m
+++ IntAirAct/NSNotificationCenter+ServiceFound.m
@@ -9,10 +9,42 @@
     return [[self defaultCenter] addHandlerForServiceFound:handler];
 }
 
++(id)addHandlerForServiceLost:(SDServiceHandler)handler
+{
+    return [[self defaultCenter] addHandlerForServiceLost:handler];
+}
+
++(id)addHandlerForServiceDiscoveryError:(SDErrorHandler)handler
+{
+    return [[self defaultCenter] addHandlerForServiceDiscoveryError:handler];
+}
+
 -(id)addHandlerForServiceFound:(SDServiceHandler)handler
 {
     return [self addObserverForName:SDServiceFound object:nil queue:nil usingBlock:^(NSNotification *note) {
         if([note.object isKindOfClass:[SDService class]]) {
+            handler(note.object);
+        } else {
+            handler(nil);
+        }
+    }];
+}
+
+-(id)addHandlerForServiceLost:(SDServiceHandler)handler
+{
+    return [self addObserverForName:SDServiceLost object:nil queue:nil usingBlock:^(NSNotification *note) {
+        if([note.object isKindOfClass:[SDService class]]) {
+            handler(note.object);
+        } else {
+            handler(nil);
+        }
+    }];
+}
+
+-(id)addHandlerForServiceDiscoveryError:(SDErrorHandler)handler
+{
+    return [self addObserverForName:SDServiceDiscoveryError object:nil queue:nil usingBlock:^(NSNotification *note) {
+        if([note.object isKindOfClass:[NSDictionary class]]) {
             handler(note.object);
         } else {
             handler(nil);
