c4349a8aba9c604116b9294a90fa78939bd5ea94
Arlo Louis O'Keeffe <mailtoarlo@gmail.com>
2012-11-14 18:23:06 -0700

    Add short-hand sendRequest without handler

--- IntAirAct/IAClient.h
+++ IntAirAct/IAClient.h
@@ -6,6 +6,8 @@ typedef void (^IAResponseHandler)(IAResponse *response, NSError * error);
 
 @protocol IAClient <NSObject>
 
+-(void)sendRequest:(IARequest*)request toDevice:(IADevice*)device;
+
 -(void)sendRequest:(IARequest*)request toDevice:(IADevice*)device withHandler:(IAResponseHandler)handler;
 
 @end
