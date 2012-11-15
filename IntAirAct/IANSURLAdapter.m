1404d613b43d1e26f843f0263bb5c7cfa52ba55a
Arlo Louis O'Keeffe <ArloLOK@ase-mbp.local>
2012-11-14 22:19:11 -0700

    Always log response

--- IntAirAct/IANSURLAdapter.m
+++ IntAirAct/IANSURLAdapter.m
@@ -20,12 +20,16 @@ static const int intAirActLogLevel = IA_LOG_LEVEL_WARN; // | IA_LOG_FLAG_TRACE
     NSAssert(device != nil, @"Device should not be nil");
     NSURLRequest * nsRequest = [NSURLRequest requestWithIARequest:request andDevice:device];
     [NSURLConnection sendAsynchronousRequest:nsRequest queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse * response, NSData * data, NSError * error) {
+        if (error) {
+            IALogError(@"An error ocurred: %@", error);
+        }
+        NSHTTPURLResponse * httpURLResponse = (NSHTTPURLResponse*)response;
+        IAResponse * iaResponse = [IAResponse new];
+        iaResponse.statusCode = @(httpURLResponse.statusCode);
+        iaResponse.body = data;
+        [iaResponse.metadata addEntriesFromDictionary:httpURLResponse.allHeaderFields];
+        IALogVerbose(@"Response: %@", iaResponse);
         if (handler) {
-            NSHTTPURLResponse * httpURLResponse = (NSHTTPURLResponse*)response;
-            IAResponse * iaResponse = [IAResponse new];
-            iaResponse.statusCode = @(httpURLResponse.statusCode);
-            iaResponse.body = data;
-            [iaResponse.metadata addEntriesFromDictionary:httpURLResponse.allHeaderFields];
             handler(iaResponse, error);
         }
     }];
