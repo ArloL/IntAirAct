8b654cf25452fe78ce49758c06f6789b2b18a12b
Arlo Louis O'Keeffe <mailtoarlo@gmail.com>
2012-11-16 12:41:56 -0700

    Added tests for IADevice

---
+++ IntAirActTests/IADeviceTests.h
@@ -0,0 +1,5 @@
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface IADeviceTests : SenTestCase
+
+@end
---
+++ IntAirActTests/IADeviceTests.m
@@ -0,0 +1,109 @@
+#import "IADeviceTests.h"
+
+#import <CocoaLumberjack/DDLog.h>
+#import <CocoaLumberjack/DDTTYLogger.h>
+#import <IntAirAct/IntAirAct.h>
+
+// Log levels : off, error, warn, info, verbose
+static const int ddLogLevel = LOG_LEVEL_VERBOSE;
+
+@implementation IADeviceTests
+
+-(void)logging
+{
+    static dispatch_once_t once;
+    dispatch_once(&once, ^ {
+        [DDLog addLogger:[DDTTYLogger sharedInstance]];
+        [[DDTTYLogger sharedInstance] setColorsEnabled:YES];
+    });
+}
+
+-(void)setUp
+{
+    [super setUp];
+
+    // Set-up code here.
+    [self logging];
+}
+
+-(void)tearDown
+{
+    // Tear-down code here.
+
+    [super tearDown];
+}
+
+- (void)testConstructor
+{
+    NSString * name = @"name";
+    NSString * host = @"host";
+    NSInteger port = 8080;
+    NSSet * supportedRoutes = [NSSet new];
+    IADevice * service = [IADevice deviceWithName:name host:host port:port supportedRoutes:supportedRoutes];
+    STAssertEquals(name, service.name, @"Name should be the same");
+    STAssertEquals(host, service.host, @"Hostname should be the same");
+    STAssertEquals(port, service.port, @"Port should be the same");
+    STAssertEquals(supportedRoutes, service.supportedRoutes, @"TXTRecord should be the same");
+}
+
+- (void)testEquals
+{
+    NSString * name = @"name";
+    NSString * host = @"host";
+    NSInteger port = 8080;
+    NSSet * supportedRoutes = [NSSet new];
+    IADevice * service = [IADevice deviceWithName:name host:host port:port supportedRoutes:supportedRoutes];
+    IADevice * other = [IADevice deviceWithName:name host:host port:port supportedRoutes:supportedRoutes];
+    STAssertEqualObjects(service, other, @"Should be equal");
+    STAssertEquals(service.hash, other.hash, @"Hashes should be equal");
+}
+
+- (void)testEqualsWithSelf
+{
+    NSString * name = @"name";
+    NSString * host = @"host";
+    NSInteger port = 8080;
+    NSSet * supportedRoutes = [NSSet new];
+    IADevice * service = [IADevice deviceWithName:name host:host port:port supportedRoutes:supportedRoutes];
+    STAssertTrue([service isEqual:service], @"Should be equal");
+    STAssertEquals(service.hash, service.hash, @"Hashes should be equal");
+}
+
+- (void)testEqualsFails
+{
+    NSString * name = @"name";
+    NSString * differentName = @"differentName";
+
+    NSString * host = @"host";
+    NSInteger port = 8080;
+    NSSet * supportedRoutes = [NSSet new];
+    IADevice * service = [IADevice deviceWithName:name host:host port:port supportedRoutes:supportedRoutes];
+    IADevice * other = [IADevice deviceWithName:differentName host:host port:port supportedRoutes:supportedRoutes];
+    STAssertFalse([service isEqual:other], @"Should not be equal");
+    STAssertFalse(service.hash == other.hash, @"Hashes should not be equal");
+}
+
+- (void)testEqualsWithDifferentObject
+{
+    NSString * name = @"name";
+    NSString * host = @"host";
+    NSInteger port = 8080;
+    NSSet * supportedRoutes = [NSSet new];
+    IADevice * service = [IADevice deviceWithName:name host:host port:port supportedRoutes:supportedRoutes];
+
+    NSString * other = @"name";
+    STAssertFalse([service isEqual:other], @"Should not be equal");
+    STAssertFalse(service.hash == other.hash, @"Hashes should not be equal");
+}
+
+- (void)testDescription
+{
+    NSString * name = @"name";
+    NSString * host = @"host";
+    NSInteger port = 8080;
+    NSSet * supportedRoutes = [NSSet new];
+    IADevice * service = [IADevice deviceWithName:name host:host port:port supportedRoutes:supportedRoutes];
+    STAssertNotNil(service.description, @"Description should not be nil");
+}
+
+@end
