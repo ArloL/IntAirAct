38c57eef1732e1ec633f7bec85276e818065785c
Arlo Louis O'Keeffe <mailtoarlo@gmail.com>
2012-11-16 18:19:03 -0700

    Added tests for DeSerialization

--- IntAirActTests/IADeserializationTests.m
+++ IntAirActTests/IADeserializationTests.m
@@ -4,6 +4,8 @@
 #import <CocoaLumberjack/DDTTYLogger.h>
 #import <IntAirAct/IntAirAct.h>
 
+#import "IANumber.h"
+
 // Log levels : off, error, warn, info, verbose
 static const int ddLogLevel = LOG_LEVEL_VERBOSE;
 
@@ -31,6 +33,44 @@ static const int ddLogLevel = LOG_LEVEL_VERBOSE;
     // Tear-down code here.
 
     [super tearDown];
+}
+
+- (void)testConstructor
+{
+    NSData * body = [NSData new];
+    IADeSerialization * deSerialization = [[IADeSerialization alloc] initWithBody:body];
+    STAssertEquals(body, deSerialization.body, @"Body should be the same");
+}
+
+- (void)testBodyAsWithEmptyBody
+{
+    NSString * body = @"example string";
+    IADeSerialization * deSerialization = [[IADeSerialization alloc] init];
+    [deSerialization setBodyWithString:body];
+    STAssertEqualObjects(body, deSerialization.bodyAsString, @"Should be 'example string'");
+}
+
+- (void)testsetBodyWithWithAnNSNumber
+{
+    IADeSerialization * deSerialization = [[IADeSerialization alloc] init];
+    [deSerialization setBodyWith:@50];
+    STAssertEqualObjects(@"50", deSerialization.bodyAsString, @"Should be 'example string'");
+}
+
+- (void)testsetBodyWithWithAnIANumber
+{
+    IANumber * number = [IANumber new];
+    number.number = @50;
+    IADeSerialization * deSerialization = [[IADeSerialization alloc] init];
+    [deSerialization setBodyWith:number];
+    NSString * expected = @"{\"number\":50}";
+    STAssertEqualObjects(expected, deSerialization.bodyAsString, @"Should be the same");
+}
+
+- (void)testDescription
+{
+    IADeSerialization * deSerialization = [IADeSerialization new];
+    STAssertNotNil(deSerialization.description, @"Description should not be nil");
 }
 
 @end
