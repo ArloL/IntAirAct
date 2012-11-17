cdec4e4d9dac44d3d513fec9cd7efb967fc1e59e
Arlo Louis O'Keeffe <mailtoarlo@gmail.com>
2012-11-17 15:30:50 -0700

    Use new NSOperationQueue instead of mainQueue

--- IntAirAct/IANSURLAdapter.m
+++ IntAirAct/IANSURLAdapter.m
@@ -19,7 +19,7 @@ static const int intAirActLogLevel = IA_LOG_LEVEL_WARN; // | IA_LOG_FLAG_TRACE
 {
     NSAssert(device != nil, @"Device should not be nil");
     NSURLRequest * nsRequest = [NSURLRequest requestWithIARequest:request andDevice:device];
-    [NSURLConnection sendAsynchronousRequest:nsRequest queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse * response, NSData * data, NSError * error) {
+    [NSURLConnection sendAsynchronousRequest:nsRequest queue:[NSOperationQueue new] completionHandler:^(NSURLResponse * response, NSData * data, NSError * error) {
         if (error) {
             IALogError(@"An error ocurred: %@", error);
         }
