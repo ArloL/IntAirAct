d5993e8b0e263fd76a916d1ac9c048449c78bdd9
Arlo Louis O'Keeffe <mailtoarlo@gmail.com>
2012-11-18 15:48:03 -0700

    Added IANSURLAdapter tests

---
+++ IntAirActTests/IANSURLAdapterTests.h
@@ -0,0 +1,5 @@
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface IANSURLAdapterTests : SenTestCase
+
+@end
---
+++ IntAirActTests/IANSURLAdapterTests.m
@@ -0,0 +1,38 @@
+#import "IANSURLAdapterTests.h"
+
+#import <CocoaLumberjack/DDLog.h>
+#import <CocoaLumberjack/DDTTYLogger.h>
+#import <IntAirAct/IntAirAct.h>
+
+#import "NSURL+QueryParameters.h"
+
+// Log levels : off, error, warn, info, verbose
+static const int ddLogLevel = LOG_LEVEL_VERBOSE;
+
+@implementation IANSURLAdapterTests
+
+-(void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+    [DDLog addLogger:[DDTTYLogger sharedInstance]];
+    [[DDTTYLogger sharedInstance] setColorsEnabled:YES];
+}
+
+-(void)tearDown
+{
+    // Tear-down code here.
+    [DDLog removeAllLoggers];
+    
+    [super tearDown];
+}
+
+-(void)testURLQueryParametersWithEmptyString
+{
+    NSURL * expected = [NSURL URLWithString:@"http://example.com/"];
+    NSURL * actual = [expected URLByAppendingQueryString:@""];
+    STAssertEqualObjects(actual, expected, nil);
+}
+
+@end
