36169845f118a73d0933653c3a447419f323e471
Arlo O'Keeffe <mailtoarlo@gmail.com>
2013-11-03 09:30:02 +0100

    Add describing comments to IAResponse

--- IntAirAct/IAResponse.h
+++ IntAirAct/IAResponse.h
@@ -2,9 +2,23 @@
 
 @class IAIntAirAct;
 
+/**
+ Represents a response.
+ */
 @interface IAResponse : IADeSerialization
 
+/**
+ The status code of the response.
+
+ This is typically a HTTP status code, e.g. @404.
+ 
+ The default value is @200.
+ */
 @property (nonatomic, strong) NSNumber * statusCode;
+
+/**
+ The metadata of the response.
+ */
 @property (nonatomic, strong) NSMutableDictionary * metadata;
 
 @end
